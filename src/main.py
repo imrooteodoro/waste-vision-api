9ac7657815e6a7349b5d3d3726e99e449dc3e4b6
imrooteodoro <adelsonteodoro67@gmail.com>
2025-11-30 01:07:50 -0300

    feat: include multimodal inference route in FastAPI application

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from src.routes.inference import router as inference_route
+from src.routes.multimodal_inference import router as multimodal_route
 
 app = FastAPI()
 
@@ -11,3 +12,4 @@ async def hello():
 
 
 app.include_router(prefix="/api", router=inference_route)
+app.include_router(prefix="/api", router=multimodal_route)
